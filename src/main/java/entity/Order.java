40f49070c3dd05cb2d98921c5cbf332b60efd688
Yrol <yrollgayanth@yahoo.com>
2022-11-23 08:34:05 +1100

    Adding Postgres support and creating the entities

---
+++ src/main/java/entity/Order.java
@@ -0,0 +1,37 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String orderTrackingNumber;
+    private int totalQuantity;
+    private BigDecimal totalPrice;
+    private String status;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+    private long shoppingCartId;
+}
